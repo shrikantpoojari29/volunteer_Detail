59038bd461e6641d91a40b49e7a26a7405ac65ad
shrikantpoojari29 <140492561+shrikantpoojari29@users.noreply.github.com>
2023-10-04 14:01:51 +0530

    first commit

---
+++ backend/config.js
@@ -0,0 +1,9 @@
+export const PORT = 5555;
+export const mongoDBURL =
+  'mongodb+srv://shrikant:shrikant@cluster0.axupbbl.mongodb.net/?retryWrites=true&w=majority';
+
+//export const mongoDBURL =
+  //'mongodb+srv://root:root@books-store-mern.c7ba8z1.mongodb.net/books-collection?retryWrites=true&w=majority';
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
